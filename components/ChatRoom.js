4d34ed088f6827b576fb8278098f965a2f0db5c6
Christian Kirschberg <ckirschberg@gmail.com>
2021-02-16 16:09:02 +0100

    .

--- components/ChatRoom.js
+++ components/ChatRoom.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const ChatRoom = props => {
+    const navigation = useNavigation(); 
+
    return (
     <View>
         <Text>{props.chatroom.name}</Text>
+        <Button title="Navigate somewhere" 
+            onPress={() => navigation.navigate("nameOfNavigationRouteEgMenu")} />
     </View>
    );
 }
